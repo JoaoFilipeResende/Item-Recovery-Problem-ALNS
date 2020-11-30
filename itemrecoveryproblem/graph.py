1a27d4f2d5aa649dfc7e44e05888892255c31aec
Kagehiko <japankage@gmail.com>
2020-11-30 21:49:02 +0000

    Finished parser

--- itemrecoveryproblem/graph.py
+++ itemrecoveryproblem/graph.py
@@ -1,11 +1,18 @@
+import numpy as np
 
 
 class Graph:
-    def __init__(self, number_of_nodes):
-        self.n_nodes = number_of_nodes
+    def __init__(self, number_of_sites):
+        self.n_nodes = number_of_sites + 1
+        self.items_at_nodes = {}
 
-        for node in range(1, self.n_nodes):
+        self.transitions = np.full((self.n_nodes, self.n_nodes), np.inf)
+
+        for node in range(1, self.n_nodes + 1):
             self.items_at_nodes[node] = []
 
     def add_item_to_node(self, node, item_size):
         self.items_at_nodes[node].append(item_size)
+
+    def add_edge(self, node_a, node_b, cost):
+        self.transitions[node_a, node_b] = cost
--- itemrecoveryproblem/itemrecoveryproblem.py
+++ itemrecoveryproblem/itemrecoveryproblem.py
@@ -11,7 +11,6 @@ class ItemRecoveryProblem:
         self.robot_capacity = None
         self.items = None
         self.number_edges = None
-        self.transitions = None
 
     def load_file(self, path):
         # File is case-insensitive, whitespace not significant, \r\n or \r or \n all accepted, empty lines are ignored
@@ -24,48 +23,46 @@ class ItemRecoveryProblem:
         if not re.match(r"cargosize:\d+$", file[1]):
             raise Exception('Second line of instance file must be "CargoSize: x" where "x" is the maximum cargo '
                             'the robot can carry at any given time')
-        if not re.match(r"nodeswithitems:\d+$", file[2]):
-            raise Exception('Third line of instance file must be "NodesWithItems: x" where "x" is the number of nodes'
+        if not re.match(r"siteswithitems:\d+$", file[2]):
+            raise Exception('Third line of instance file must be "SitesWithItems: x" where "x" is the number of sites'
                             ' that contain items')
 
-        self.graph = Graph(int(file[0][6:]))
+        number_of_sites = int(file[0][6:])
+        self.graph = Graph(number_of_sites)
         self.robot_capacity = int(file[1][10:])
 
-        nodes_with_items = int(file[2][15:])
+        sites_with_items = int(file[2][15:])
 
         self.items = {}
         file_idx = 3
-        for x in range(0, nodes_with_items):
+        for x in range(0, sites_with_items):
             line = file[file_idx + x]
 
             if not re.match(r"\d:(\d+)(,\d+)*$", line):
-                raise Exception('Please specify the items at each node with the following format: "x: a,b,c,...",'
-                                ' where x is the node and a,b,c... are the sizes of the items at that node')
+                raise Exception('Please specify the items at each site with the following format: "x: a,b,c,...",'
+                                ' where x is the site and a,b,c... are the sizes of the items at that site')
 
-            node_idx = int(line.split(":")[0])
-            items_in_node = [int(x) for x in line.split(":")[1].split(",")]
+            site_idx = int(line.split(":")[0])
 
-            if node_idx > self.number_nodes:
-                raise Exception('Invalid index ' + str(node_idx) + " found in transitions, but only "
-                                + str(self.number_nodes) + " nodes exist.")
+            if site_idx == 0:
+                raise Exception('Invalid site 0 in item specification. Site 0 is the "base" and cannot have items')
 
-            self.items[node_idx] = items_in_node
-        file_idx = file_idx + nodes_with_items
+            if site_idx > number_of_sites:
+                raise Exception('Invalid site ' + str(site_idx) + ' in item specification.')
 
-        # Sanity check - No item can be bigger than the cargo
-        biggest_item_size_in_graph = 0
-        for node_items in self.items.values():
-            biggest_item_size_in_node = max(node_items)
-            if biggest_item_size_in_node > biggest_item_size_in_graph:
-                biggest_item_size_in_graph = biggest_item_size_in_node
+            for item_size in line.split(":")[1].split(","):
+                if int(item_size) < self.robot_capacity:
+                    self.graph.add_item_to_node(site_idx, int(item_size))
+                else:
+                    raise Exception('Items cannot exceed the cargo capacity.')
 
-        if re.match("edges:\d+$", file[file_idx]):
+        file_idx = file_idx + sites_with_items
+
+        if re.match("paths:\d+$", file[file_idx]):
             self.number_edges = int(file[file_idx][6:])
         else:
-            raise Exception('Number of edges of the graph must be indicated after specifying the items at each node. '
-                            'Format: "Edges: x"')
-
-        self.transitions = np.full((self.number_nodes, self.number_nodes), np.inf)
+            raise Exception('Number of paths between sites must be indicated after specifying the items at each site. '
+                            'Format: "Paths: x", where x is the number of paths between sites')
 
         file_idx += 1
         for x in range(0, self.number_edges):
@@ -73,9 +70,17 @@ class ItemRecoveryProblem:
 
             if re.match("\d+,\d+:\d+$", line):
                 line = re.split(",|:", line)
-                if int(line[0]) > (self.number_nodes-1):
-                    raise Exception('Invalid edge. Node ' + line[0] + ' does not exist.')
+                if int(line[0]) > number_of_sites:
+                    raise Exception('Invalid path. Site ' + line[0] + ' does not exist.')
+                if int(line[1]) > number_of_sites:
+                    raise Exception('Invalid path. Site ' + line[1] + ' does not exist.')
+
+                self.graph.add_edge(int(line[0]), int(line[1]), int(line[2]))
+                self.graph.add_edge(int(line[1]), int(line[0]), int(line[2]))
+
             else:
                 raise Exception('Invalid format for specifying an edge. Format: "x,y: z", where x and y'
                                 ' are nodes and z is the cost of the edge')
 
+        for site in range(0, number_of_sites + 1):
+            self.graph.add_edge(site, site, 0)
