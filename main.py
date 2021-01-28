3b525b351102ba600f11e2ba230c5e7449d1a5f4
Kagehiko <japankage@gmail.com>
2021-01-28 05:41:21 +0000

    Renamed instances and other minor stuff

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ from alns import ALNS
 from alns.criteria import HillClimbing, SimulatedAnnealing
 from alnsoperators.operators import *
 import matplotlib.pyplot as plt
+import time
+import sys
 
 seed = 12345
+iterations = 200
+instance_to_run = 0  # Only used when not run with command line arguments
 
 if __name__ == '__main__':
     random_state = rnd.RandomState(seed)
 
     irp = ItemRecoveryProblem()
-    irp.load_file("./instances/instance_38")
+    try:
+        instance_path = "./instances/" + str(sys.argv[1])
+    except IndexError:
+        instance_path = "./instances/" + str(instance_to_run)
+        print("Executing hard-coded instance:", instance_to_run)
+
+    irp.load_file(instance_path)
 
     alns = ALNS(random_state)
     alns.add_destroy_operator(remove_rand_pos)
@@ -22,18 +32,22 @@ if __name__ == '__main__':
     alns.add_repair_operator(greedy_repair)
 
     initial_solution = greedy_repair(Solution(irp), random_state)
-    print("Initial solution:", initial_solution.get_cost())
-    criterion = HillClimbing()
-    #criterion = SimulatedAnnealing(100, 10, 5, method='linear')
+    print(instance_path)
+    print("Initial solution:", int(initial_solution.get_cost()))
+    #criterion = HillClimbing()
+    criterion = SimulatedAnnealing(100, 5, 5, method='linear')
+    start_time = time.time()
     result = alns.iterate(initial_solution, [3, 2, 1, 0.5], 0.8,
-                          criterion, iterations=30, collect_stats=True)
+                          criterion, iterations=iterations, collect_stats=False)
+    end_time = time.time()
     solution = result.best_state
-    print("Best solution:", solution.objective())
-
-    _, ax = plt.subplots(figsize=(12, 6))
-    result.plot_objectives(ax=ax, lw=2)
-    figure = plt.figure("operator_counts", figsize=(14, 6))
-    figure.subplots_adjust(bottom=0.15, hspace=.5)
-    result.plot_operator_counts(figure=figure, title="Operator diagnostics", legend=["Best", "Better", "Accepted"])
+    print("Best solution:", int(solution.objective()))
+    print("Time taken (s):", int(end_time - start_time))
+    print("Iterations:", iterations)
+    #_, ax = plt.subplots(figsize=(12, 6))
+    #result.plot_objectives(ax=ax, lw=2)
+    #figure = plt.figure("operator_counts", figsize=(14, 6))
+    #figure.subplots_adjust(bottom=0.15, hspace=.5)
+    #result.plot_operator_counts(figure=figure, title="Operator diagnostics", legend=["Best", "Better", "Accepted"])
 
     print("Is best solution valid?", solution.check_validity()[0])
